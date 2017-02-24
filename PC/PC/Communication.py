d039989ba94a308380b8277b2266e6378862ba5e
Hieu Nguyen (Laptop) <hnguye19@hawk.iit.edu>
2017-02-24 00:45:26 -0600

    .

--- PC/PC/Communication.py
+++ PC/PC/Communication.py
@@ -14,19 +14,18 @@ class Communication(socket.socket):
     def __str__(self):
         return self.host+":"+str(self.port)
 
-    def connect(self,password):
+    def connect(self):
         print("Connecting to ", str(self))
         timer1=Timer()
         timer1.resetTimer()
-        while self.privateConnect(password)==False and timer1.timer()<30.0:pass #emmty while loop for 30 seconds 
+        while self.privateConnect()==False and timer1.timer()<30.0:pass #emmty while loop for 30 seconds 
         if self.connected==False:print("Connection to "+str(self)+" failed.")
         return self.connected
 
-    def privateConnect(self,password):# to be called by self.connect()
+    def privateConnect(self):# to be called by self.connect()
         try:
             self.s.connect((self.host, self.port))
             print("Connection to "+self.host+":"+str(self.port)+" established.")
-            sendInt(password)
             self.connected=True
         except:
             self.connected=False
