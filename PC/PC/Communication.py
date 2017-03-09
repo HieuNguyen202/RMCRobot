beeff33330fe3aecd25dfc138fe23a9d29905f82
Hieu Nguyen <hnguye19@hawk.iit.edu>
2017-03-09 15:59:00 +0700

    .

--- PC/PC/Communication.py
+++ PC/PC/Communication.py
@@ -16,8 +16,7 @@ class Communication(socket.socket):
         self.connected=False
         self.dashboard=dashboard
         self.speedScale=Scale(-127,127,0,math.pow(2,self.message.numData1Digit)-1)
-        self.prevCommand=-1
-        self.prevPrevCommand=-1
+        self.prevMessageInt=-1
         self.commandDict={'stop':0,'drive':1, 'dig':2,'arm':3,'hand':4}
 
     def __str__(self):
@@ -47,68 +46,7 @@ class Communication(socket.socket):
     def send(self, message):
         self.s.send(message)
     def close(self):
-        self.s.close()
-
-    def oldtellPi(self,command, data1, data2=None): # 1 byte message
-        if data2==None:#single command
-            if data1!=self.prevCommand:
-                if command=='left': self.sendInt(int(self.speedScale.unScale(data1,0,63))) 
-                elif command=='right': self.sendInt(int(self.speedScale.unScale(data1,64,127)))      
-                elif command=='arm': self.sendInt(int(self.speedScale.unScale(data1,127,191)))                    
-                elif command=='hand': self.sendInt(int(self.speedScale.unScale(data1,192,255)))    
-                self.prevPrevCommand=self.prevCommand
-                self.prevCommand=data1              
-        else: #double command
-            if data1!=self.prevPrevCommand or data2!=self.prevCommand:
-                if command=='straight':
-                    self.sendInt(int(self.speedScale.unScale(data1,0,63)))
-                    self.sendInt(int(self.speedScale.unScale(data2,64,127)))
-                elif command=='drive':
-                    self.sendInt(int(self.speedScale.unScale(data1,0,63)))
-                    self.sendInt(int(self.speedScale.unScale(data2,64,127)))
-                elif command=='dig':
-                    self.sendInt(int(self.speedScale.unScale(data1,127,191)))
-                    self.sendInt(int(self.speedScale.unScale(data2,192,255)))
-                self.prevPrevCommand=data1
-                self.prevCommand=data2
-    def oldtellPi2(self, command, data1, data2=None): # 2 byte message
-        if data2==None:#single command
-            if data1!=self.prevCommand:
-                if command=='left': self.sendInt2(int(self.speedScale.unScale(data1,0,255))) 
-                elif command=='right': self.sendInt2(int(self.speedScale.unScale(data1,256,511)))      
-                elif command=='arm': self.sendInt2(int(self.speedScale.unScale(data1,127,191)))                    
-                elif command=='hand': self.sendInt2(int(self.speedScale.unScale(data1,192,255)))    
-                self.prevPrevCommand=self.prevCommand
-                self.prevCommand=data1              
-        else: #double command
-            if data1!=self.prevPrevCommand or data2!=self.prevCommand:
-                if command=='drive':
-                    self.sendInt2(int(self.speedScale.unScale(data1,0,255)))
-                    self.sendInt2(int(self.speedScale.unScale(data2,256,511)))
-                self.prevPrevCommand=data1
-                self.prevCommand=data2
-    def oldsendInt(self,number): #number from 0 to 255
-        hexString=format(number, '02x')#convert int to binary
-        message=binascii.hexlify(binascii.unhexlify(hexString))#convert int to binary
-        self.send(message)
-    def oldsendInt2(self,number): #number from 0 to 65535
-        hexString=format(number, '04x')#convert int to binary
-        message=binascii.hexlify(binascii.unhexlify(hexString))#convert int to binary
-        self.send(emssage)
-    def oldsendInt(self,number1,number2): #number from 0 to 255
-        hexString1=format(number1, '02x')#convert int to binary
-        hexString2=format(number2, '02x')
-        bin1=binascii.hexlify(binascii.unhexlify(hexString1))#convert int to binary
-        bin2=binascii.hexlify(binascii.unhexlify(hexString2))
-        conbinedBin=bin1 + bin2
-        print(conbinedBin)
-        self.send(conbinedBin)
-    def oldtellPi(self, command, data1, data2): # 2 byte message        
-        if command=='drive':
-            lspeed=int(self.speedScale.unScale(data1,0,255))
-            rspeed=int(self.speedScale.unScale(data2,0,255))
-            self.sendInt(lspeed,rspeed)
-    
+        self.s.close()   
     def tellPi(self, rawCommand, rawData1=None, rawData2=None): # 2 byte message        
         commandInt=commandDict[rawCommand]
         if commandInt==0:
@@ -127,7 +65,68 @@ class Communication(socket.socket):
             data1Int=speedScale.scale(rawData1)
             data2Int=0
         self.message.setValues(commandInt,data1Int,data2Int)
-        self.send(message.getBin())
+        thisMessageInt=self.message.getInt()
+        if thisMessageInt != self.prevMessageInt: 
+            self.send(message.getBin())
+            self.prevMessageInt=thisMessageInt
 
-        #self.prevPrevCommand=data1
-        #self.prevCommand=data2
+
+    #def oldtellPi(self,command, data1, data2=None): # 1 byte message
+    #    if data2==None:#single command
+    #        if data1!=self.prevCommand:
+    #            if command=='left': self.sendInt(int(self.speedScale.unScale(data1,0,63))) 
+    #            elif command=='right': self.sendInt(int(self.speedScale.unScale(data1,64,127)))      
+    #            elif command=='arm': self.sendInt(int(self.speedScale.unScale(data1,127,191)))                    
+    #            elif command=='hand': self.sendInt(int(self.speedScale.unScale(data1,192,255)))    
+    #            self.prevPrevCommand=self.prevCommand
+    #            self.prevCommand=data1              
+    #    else: #double command
+    #        if data1!=self.prevPrevCommand or data2!=self.prevCommand:
+    #            if command=='straight':
+    #                self.sendInt(int(self.speedScale.unScale(data1,0,63)))
+    #                self.sendInt(int(self.speedScale.unScale(data2,64,127)))
+    #            elif command=='drive':
+    #                self.sendInt(int(self.speedScale.unScale(data1,0,63)))
+    #                self.sendInt(int(self.speedScale.unScale(data2,64,127)))
+    #            elif command=='dig':
+    #                self.sendInt(int(self.speedScale.unScale(data1,127,191)))
+    #                self.sendInt(int(self.speedScale.unScale(data2,192,255)))
+    #            self.prevPrevCommand=data1
+    #            self.prevCommand=data2
+    #def oldtellPi2(self, command, data1, data2=None): # 2 byte message
+    #    if data2==None:#single command
+    #        if data1!=self.prevCommand:
+    #            if command=='left': self.sendInt2(int(self.speedScale.unScale(data1,0,255))) 
+    #            elif command=='right': self.sendInt2(int(self.speedScale.unScale(data1,256,511)))      
+    #            elif command=='arm': self.sendInt2(int(self.speedScale.unScale(data1,127,191)))                    
+    #            elif command=='hand': self.sendInt2(int(self.speedScale.unScale(data1,192,255)))    
+    #            self.prevPrevCommand=self.prevCommand
+    #            self.prevCommand=data1              
+    #    else: #double command
+    #        if data1!=self.prevPrevCommand or data2!=self.prevCommand:
+    #            if command=='drive':
+    #                self.sendInt2(int(self.speedScale.unScale(data1,0,255)))
+    #                self.sendInt2(int(self.speedScale.unScale(data2,256,511)))
+    #            self.prevPrevCommand=data1
+    #            self.prevCommand=data2
+    #def oldsendInt(self,number): #number from 0 to 255
+    #    hexString=format(number, '02x')#convert int to binary
+    #    message=binascii.hexlify(binascii.unhexlify(hexString))#convert int to binary
+    #    self.send(message)
+    #def oldsendInt2(self,number): #number from 0 to 65535
+    #    hexString=format(number, '04x')#convert int to binary
+    #    message=binascii.hexlify(binascii.unhexlify(hexString))#convert int to binary
+    #    self.send(emssage)
+    #def oldsendInt(self,number1,number2): #number from 0 to 255
+    #    hexString1=format(number1, '02x')#convert int to binary
+    #    hexString2=format(number2, '02x')
+    #    bin1=binascii.hexlify(binascii.unhexlify(hexString1))#convert int to binary
+    #    bin2=binascii.hexlify(binascii.unhexlify(hexString2))
+    #    conbinedBin=bin1 + bin2
+    #    print(conbinedBin)
+    #    self.send(conbinedBin)
+    #def oldtellPi(self, command, data1, data2): # 2 byte message        
+    #    if command=='drive':
+    #        lspeed=int(self.speedScale.unScale(data1,0,255))
+    #        rspeed=int(self.speedScale.unScale(data2,0,255))
+    #        self.sendInt(lspeed,rspeed)
