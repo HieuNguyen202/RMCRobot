ae82b2a44fe8979a647160c2ce05abe6141c499e
Hieu Nguyen <hnguye19@hawk.iit.edu>
2017-04-21 07:20:37 +0700

    Added ArduinoMotor Solution

---
+++ ArduinoMotor/ArduinoMotor/ArduinoMotor.ino
@@ -0,0 +1,160 @@
+// Adafruit Motor shield library
+// copyright Adafruit Industries LLC, 2009
+// this code is public domain, enjoy!
+
+#include "I2C.h"
+#include "Timer.h"
+#include "SpeedEncoder.h"
+#include "Encoderr.h"
+#include "Box.h"
+#include <Wire.h>
+#include <AFMotor.h>
+#include <digitalWriteFast.h>
+#include "Sabertooth.h"
+#include "Timer.h"
+#define SLAVE_ADDRESS 0x07
+
+int leftTargetSpeed;
+int rightTargetSpeed = 300;
+int leftCurrentSpeed;
+int rightCurrentSpeed;
+int power;
+TimerClass timer;
+TimerClass timer2;
+SpeedEncoderClass encoder12;
+I2CClass i2c12(7);
+Sabertooth ST(130);
+
+//PID
+int previous_error = 0;
+int integral = 0;
+int dt = 20;
+double Kp = 0.07;
+double Kd = 2;
+double Ki = 0.0001;
+
+
+
+
+
+//0 for going straight ahead, 200 for spining
+void setup() {
+	Serial.begin(9600);           // set up serial library at 9600 bps
+	Serial.println("Motor test begin!");
+	//Encoder setup
+	encoder12.attach(rise1, rise2); //attachInterupt for pin 2 and 3
+									//i2c setup
+	i2c12.attach(receiveData, sendData);
+
+	//Sabertooth
+	//SabertoothTXPinSerial.begin(9600);
+	//ST.drive(0);
+	//ST.turn(0);
+	ST.drive(50);
+	ST.turn(50);
+	timer.reset();
+	timer2.reset();
+	pinMode(0, OUTPUT);  // declare the ledPin as an OUTPUT
+	pinMode(1, OUTPUT);  // declare the ledPin as an OUTPUT
+
+}
+void loop() {
+	encoder12.updateSpeed();
+	//Motor
+	//drive(-75, -75);
+
+	//Encoder
+	if (timer.getTime()>100)
+	{
+		//Serial.println("hieu"+String(Kd*derivative));
+		int pot0 = analogRead(0);
+		int pot1 = analogRead(1);
+		//Ki = pot0/7500.;
+		//Kd = pot1;
+		Serial.println("V 1: " + String(encoder12.v1) + " Power: " + power + " Ki: " + String(Ki));
+
+	}
+	//ST.drive(127);
+	//i2c
+	//Serial.println("leftTargetSpeed: " + String(i2c12.leftTargetSpeed) + " rightTargetSpeed: " + String(i2c12.rightTargetSpeed) );
+
+	//Sabertooth
+
+	//// Don't turn. Ramp from going backwards to going forwards, waiting 20 ms (1/50th of a second) per value.
+
+	if (timer.getTime() > dt)
+	{
+		int error = rightTargetSpeed - encoder12.v1;
+
+		integral = integral + error*dt;
+		double derivative = (error - previous_error) / (double)dt;
+		power = power + Kp*error + Ki*integral + Kd*derivative;
+		//Serial.println("Erro " + String(error) + " Drev " + String(Kd*derivative));
+		previous_error = error;
+		if (power>127)
+		{
+			power = 127;
+		}
+		ST.drive(power);
+		ST.turn(20);
+	}
+	//if (timer.getTime() > 1)
+	//{
+	//	long currentSpeed = encoder12.v1;
+	//	if (currentSpeed<rightTargetSpeed-50)
+	//	{
+	//		power++;
+	//	}
+	//	else if (currentSpeed>rightTargetSpeed+50)
+	//	{
+	//		power--;
+	//	}
+	//	else
+	//	{
+
+	//	}
+	//	if (power>127)
+	//	{
+	//		power = 127;
+	//	}
+	//	else if (power<-127)
+	//	{
+	//		power = -127;
+	//	}
+	//	ST.drive(power);
+	//	timer.reset();
+	//}
+
+	//// Now, let's use a power level of 20 (out of 127) forward.
+	//// This way, our turning will have a radius.
+	//ST.drive(20);
+
+	//// Ramp turning from full left to full right SLOWLY by waiting 50 ms (1/20th of a second) per value.
+	//for (power = -127; power <= 127; power++)
+	//{
+	//	ST.turn(power);
+	//	delay(50);
+	//}
+
+	//// Now stop turning, and stop driving.
+	//ST.turn(0);
+	//ST.drive(0);
+
+	//// Wait a bit. This is so you can catch your robot if you want to. :-)
+	//delay(5000);
+}
+//Encoder
+void rise1() {
+	encoder12.rise1();
+}
+void rise2() {
+	encoder12.rise2();
+}
+//i2c
+void receiveData(int byteCount) {
+	i2c12.receiveData(byteCount);
+}
+void sendData() {//fix this
+	i2c12.sendData();
+}
+//motor
